aef597d33b674007206a2facdfca7d79a0a942aa
Brandutchmen <Brandutchmen@users.noreply.github.com>
2017-10-07 20:08:54 -0500

    Rev1

---
+++ autonomous/drop.py
@@ -0,0 +1,26 @@
+from robotpy_ext.autonomous import StatefulAutonomous, timed_state, state
+import wpilib
+class DriveForward(StatefulAutonomous):
+    
+    DEFAULT = False
+    MODE_NAME = 'Drop'
+    
+    def initialize(self):
+        pass
+    
+    @timed_state(duration=0.5, next_state='drive_forward', first=True)
+    def drive_wait(self):
+        self.drive.arcadeDrive(0,0)
+    @timed_state(duration=4, next_state='drop')
+    def drive_forward(self):
+        self.drive.arcadeDrive(0.5, 0)
+    @timed_state(duration=0.5, next_state='reverse')
+    def drop(self):
+        self.gearDrop.set(wpilib.DoubleSolenoid.Value.kForward)
+        self.drive.arcadeDrive(0, 0)
+    @timed_state(duration=0.5, next_state='stop')
+    def reverse(self):
+        self.drive.arcadeDrive(-0.5, 0)
+    @state()
+    def stop(self):
+        self.drive.arcadeDrive(0,0,0)
---
+++ robot.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import wpilib
+from wpilib import RobotDrive
+import wpilib.buttons
+from robotpy_ext.autonomous import AutonomousModeSelector
+
+
+
+class MyRobot(wpilib.IterativeRobot):
+    
+    frontLeftChannel  = 2
+    rearLeftChannel   = 1
+    frontRightChannel = 3
+    rearRightChannel  = 4
+    
+    
+    
+    joystickChannel   = 0
+    
+    
+    def robotInit(self):
+        '''Robot initialization function'''
+        
+        if not wpilib.RobotBase.isSimulation():
+            import ctre
+            
+            self.FLC = ctre.CANTalon(self.frontLeftChannel)
+            self.FRC = ctre.CANTalon(self.frontRightChannel)
+            self.RRC = ctre.CANTalon(self.rearRightChannel)
+            self.RLC = ctre.CANTalon(self.rearLeftChannel)
+        
+        else:
+            self.RRC = wpilib.Talon(self.rearRightChannel)
+            self.RLC = wpilib.Talon(self.rearLeftChannel)
+            self.FRC = wpilib.Talon(self.frontRightChannel)
+            self.FLC = wpilib.Talon(self.frontLeftChannel)
+        
+        self.stick = wpilib.Joystick(self.joystickChannel)
+        self.winch_motor1 = wpilib.Talon(7)
+        self.winch_motor2 = wpilib.Talon(8)
+        
+        self.robotDrive = wpilib.RobotDrive(self.FLC,
+                                            self.RLC,
+                                            self.FRC,
+                                            self.RRC)
+                                            
+        wpilib.CameraServer.launch()
+        
+        #Goto 10.44.80.2:1181
+        #to view the cameras without HTML page
+            
+        self.robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, True)
+                                            
+        self.robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, True)
+                                            
+                        
+        self.winch_forward = wpilib.buttons.JoystickButton(self.stick, 5)
+        self.winch_backward = wpilib.buttons.JoystickButton(self.stick, 6)
+                                            
+        self.flip = wpilib.buttons.JoystickButton(self.stick, 1)
+        self.open_double = wpilib.buttons.JoystickButton(self.stick, 3)
+        self.close_double = wpilib.buttons.JoystickButton(self.stick, 4)
+                                            
+        self.single_solenoid = wpilib.Solenoid(1)
+        self.double_solenoid = wpilib.DoubleSolenoid(2,3)
+        
+        self.dm = 2
+
+        self.xboxMec = wpilib.buttons.JoystickButton(self.stick, 8)
+        self.xboxTank = wpilib.buttons.JoystickButton(self.stick, 7)
+
+
+
+        self.components = {
+            'drive': self.robotDrive,
+            'gearDrop': self.double_solenoid,
+        }
+
+        self.automodes = AutonomousModeSelector('autonomous', self.components)
+
+    def autonomousPeriodic(self):
+        self.automodes.run()
+
+
+    def teleopPeriodic(self):
+
+
+        if self.xboxMec.get():
+            self.dm = 1
+        if self.xboxTank.get():
+            self.dm = 2
+        if self.dm == 1:
+            self.robotDrive.mecanumDrive_Cartesian(self.stick.getX(), self.stick.getY(), self.stick.getRawAxis(4), 0)
+        if self.dm == 2:
+            self.robotDrive.tankDrive(self.stick.getY(), self.stick.getRawAxis(5), True)
+
+
+        if (self.winch_forward.get()):
+            self.winch_motor1.set(1)
+            self.winch_motor2.set(1)
+        elif (self.winch_backward.get()):
+            self.winch_motor1.set(-1)
+            self.winch_motor2.set(-1)
+        else:
+            self.winch_motor1.set(0)
+            self.winch_motor2.set(0)
+        
+        if (self.flip.get()):
+            self.single_solenoid.set(True)
+        else:
+            self.single_solenoid.set(False)
+        
+        if (self.open_double.get()):
+            self.double_solenoid.set(wpilib.DoubleSolenoid.Value.kForward)
+            
+        elif (self.close_double.get()):
+            self.double_solenoid.set(wpilib.DoubleSolenoid.Value.kReverse)
+
+
+if __name__ == '__main__':
+    wpilib.run(MyRobot)
